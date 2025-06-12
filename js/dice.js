cfdf6a8f455f1fcb989ddcada1064d5662141dd8
Shehryar Khan <51154475+shehryar8@users.noreply.github.com>
2025-06-12 02:34:05 -0500

    Add files via upload

---
+++ js/dice.js
@@ -0,0 +1,16 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDice1 = "./images/dice" + randomNumber1.toString() + ".png";
+var randomDice2 = "./images/dice" + randomNumber2.toString() + ".png";
+
+document.querySelector(".img1").setAttribute("src", randomDice1);
+document.querySelector(".img2").setAttribute("src", randomDice2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+  document.querySelector("h1").innerHTML = "Draw";
+}
